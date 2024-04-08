43b789ed258be39d1be200724cfc73e5c603026b
José Quadrado <joseqcorreia@gmail.com>
2024-04-08 13:37:31 +0100

    C kernel

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 boot:
-	nasm -f bin bootloader.asm -o bootloader.bin
-	qemu-system-x86_64 bootloader.bin
+	nasm -f bin bootloader.asm -o bootloader.bin # build bootloader
+	i386-elf-gcc -ffreestanding -c kernel.c -o kernel.o # build kernel
+	nasm -f elf kernel_entry.asm -o kernel_entry.o # build kernel_entry (note: as ELF)
+	# Use linker to resolve extern in kernel_entry.o
+	i386-elf-ld -o kernel.bin -Ttext 0x1000 kernel.o kernel_entry.o --oformat binary
+	# Concatenate both bootloader and kernel back to back since we are reading right after the first 512bytes
+	cat bootloader.bin kernel.bin > disk.bin
+	# Run 
+	qemu-system-x86_64 -fda disk.bin
--- bootloader.asm
+++ bootloader.asm
@@ -1,7 +1,8 @@
 [org 0x7c00]
+    KERNEL_OFFSET equ 0x1000
     mov [bios_disk], dl ; store current disk set from bios in dl register
 
-    mov bp, 0x8000 ; set the stack safely away from us
+    mov bp, 0x9000 ; set the stack safely away from us
     mov sp, bp
 
     call clearScreen
@@ -13,42 +14,22 @@
     call print
     call print_nl
 
-    mov dx, 0xdead
-    call print_hex
+    call load_kernel
+    call switch_protected_mode
+    jmp $
+
+load_kernel:
+    mov bx, MSG_KERNEL_LOAD
+    call print
     call print_nl
-    
-; _loop:
-;     mov ah, 0x00 
-;     int 0x16
 
-;     cmp ah, 0x1c ; Enter
-;     je _loop
+    mov bx, KERNEL_OFFSET 
 
-;     mov ah, 0x0e
-;     int 0x10
-
-;     jmp _loop
-
-
-mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-
-; Load 2 segments of disk 
-mov dh, 2
-mov dl, [bios_disk]
-call disk_load
-
-; Print a word from first segment
-mov dx, [bx + 256]
-call print_hex
-call print_nl
-
-; Print a word from second segment
-mov dx, [bx + 512]
-call print_hex
-call print_nl
-
-call switch_protected_mode
-jmp $
+    ; Load 2 segments of disk 
+    mov dh, 2
+    mov dl, [bios_disk]
+    call disk_load
+    ret
 
 %include "utils.asm"
 %include "disk.asm"
@@ -59,20 +40,19 @@ jmp $
 BEGIN_PM:
     mov ebx, MSG_PROTECTED_MODE
     call print_string_pm
+    call KERNEL_OFFSET
 
     jmp $
 
 
 bios_disk: db 0x00
-hello: db "Hello, World", 0
 
 MSG_REAL_MODE: db "Running in 16-bit real mode", 0
 MSG_PROTECTED_MODE: db "Running in 32-bit protected mode", 0
+MSG_KERNEL_LOAD: db "Loading Kernel", 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
 
-times 256 dw 0xdead ; word = 2 bytes, 256 * 2 = 512 bytes == 1 sector!
-times 256 dw 0xbeef ; another one
 
 
